68ec4b92bba0cda7b07f5706176d1492e04c0e7c
Cox-Joshua <57378010+Cox-Joshua@users.noreply.github.com>
2025-08-07 20:52:50 +0000

    Deploying to gh-pages from @ Cox-Joshua/Cox-Joshua.github.io@1cffe1011f8662fc5d6934caf232751b62a41183 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,33 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-resume",
+          title: "Resume",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/resume/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-resume",
-          title: "Resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-hobbies",
+          title: "Hobbies",
+          description: "For fun activities I participate in",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/hobbies/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
